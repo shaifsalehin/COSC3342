b8715db2aad5c9a9d2af8fe95a58f9a5ea605bbb
shaifsalehin <shaifsalehin@gmail.com>
2020-10-21 19:49:34 -0500

    change: some error handling, print

--- server.c
+++ server.c
@@ -22,22 +22,25 @@ int main (int argc, char **argv){
     bool verbose = true;
     
     
-    /* Error checking for port number */
+    /* error checking for port number */
     if (argc <= 1){
-       fprintf(stderr, "Error:Please provide port number. \nUsage ./[Program name] [Port number] \nServer cannot be started.\n");
-          
+       fprintf(stderr, "Error:Please provide port number\n");
+       fprintf(stdout, "Usage ./Executable_name Port_number\n");
+       fprintf(stdout, "Server cannot be started\n");         
        exit(1);
     }
 
     /* check if number is typed (only checks for numbers, truncates any letters after the numbers) */
     if (isdigit(*argv[1]) == 0){
-        fprintf(stderr, "Error: Invalid port number.\nServer cannot be started.\n");
+        fprintf(stderr, "Error: Invalid port number\n");
+        fprintf(stdout, "Server cannot be started\n");
         exit(1);
     }
     
     /* check port range 1 - 65535 */
     if ((atoi(argv[1]) < 1) || (atoi(argv[1]) > 65535)){
-           fprintf(stderr, "Error: Port number must be in range 1 - 65,535.\nServer cannot be started.\n"); 
+           fprintf(stderr, "Error: Port number must be in range 1 - 65,535\n");
+           fprintf(stdout, "Server cannot be started\n"); 
            exit(1);
     }
      
@@ -45,45 +48,51 @@ int main (int argc, char **argv){
 
     port_num = atoi(argv[1]);   
      
-    /* set up our socket */
-    sock_fd = socket(AF_INET,SOCK_STREAM,0);
+    /* set up socket */
+    if (!(sock_fd = socket(AF_INET,SOCK_STREAM,0))){
+           fprintf(stderr, "Error: Socket failed\n");
+           fprintf(stdout, "Server cannot be started\n");
+           exit(1);
+    }
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(port_num);
    
-   /* creating our bind */ 
+   /* create socket bind */ 
     if (bind(sock_fd, (const struct sockaddr *)&server_addr, sizeof(server_addr))){
-            printf("Bind failed\n");
-            exit(1);
+           fprintf(stderr, "Error: Bind failed\n");
+           fprintf(stdout, "Server cannot be started\n"); 
+           exit(1);
     }
     
-    /* creating our listen */
+    /* create socket listen */
     if (listen (sock_fd, backlog)){
-            fprintf(stderr, "Listen failed\n");
-            exit (1);
+           fprintf(stderr, "Error: Listen failed\n");
+           fprintf(stdout, "Server cannot be started\n"); 
+           exit (1);
     }
 
         fprintf(stdout, "Server running at port: %u\n", port_num);
         fprintf(stdout, "Waiting for connection...\n");
         fprintf(stdout, "====================================\n");
+  
     while(1){
 
-        /* creating our accept */
-        clientConnection = accept (sock_fd, (struct sockaddr *) &server_addr, &server_len);
-        
-        /* error checking accept */
-        if (clientConnection == -1){
-            fprintf(stderr, "Accept failed\n");
-            exit (1);
+        /* create connection from client */
+        if (!(clientConnection = (accept (sock_fd, (struct sockaddr *) &server_addr, &server_len)))){
+            fprintf(stderr, "Error: Accept failed\n");
+            fprintf(stdout, "Connection cannot be established\n"); 
+  
         }else{
+
             fprintf(stdout, "Connection successful\n");
             
-            if(verbose){
-            fprintf(stdout, "Accept value: %d\n", clientConnection);
+                if(verbose){
+                    fprintf(stdout, "Accept value: %d\n", clientConnection);
             }
 
-            /* creating read function */
-            int data_read = read (clientConnection, buffer, sizeof(buffer));
+            /* create read function */
+            ssize_t data_read = read (clientConnection, buffer, sizeof(buffer));
             
             /* check for and strip new line and carriage return in buffer */ 
             buffer[strcspn(buffer, "\r\n")] = 0;
@@ -98,16 +107,18 @@ int main (int argc, char **argv){
             if(verbose){
                 printf("check string: %d\n",check_string);
 
+            /* server response to command received */
             }   
             if (check_string == 0){
                 fprintf(stdout, "Begin dealing\n");
-                sprintf(buff, "Begin dealing\n");
+                sprintf(buff, "Server: Begin dealing\n");
 
             }else{
                 fprintf(stdout, "Invalid command\n");
-                sprintf(buff, "Invalid command\n");
+                sprintf(buff, "Server: Invalid command\n");
             }
-
+            
+            /* create send function to send message to client */
             ssize_t data_sent = send(clientConnection, buff, strlen(buff), flags);
             break;
         }
