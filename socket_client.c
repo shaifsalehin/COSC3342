bfa80dbeae54f3d203d5dec0d30808c9cefedf37
shaifsalehin <shaifsalehin@gmail.com>
2020-11-01 20:18:45 -0600

    change: socket_client.c

--- socket_client.c
+++ socket_client.c
@@ -10,18 +10,21 @@
 #include <stdbool.h>
 
 void send_data(int , char * , int , int );
+int arg_check(int, char **);
 
 int main(int argc, char ** argv){
 
-        struct sockaddr_in client_addr;
+    struct sockaddr_in client_addr;
 
-        int flags, rec = 1, sock_fd =0;
-        unsigned short port_num;
-        size_t client_len = sizeof(client_addr);
-        char buffer[1024] = {0};
-        char temp[4] = "Deal"; 
-        
-        port_num = atoi(*&argv[1]);
+    int flags, rec = 1, sock_fd =0;
+    unsigned short port_num;
+    size_t client_len = sizeof(client_addr);
+    char buffer[1024] = {0};
+    char temp[4] = "Deal"; 
+    
+    if (arg_check(argc, argv) == -1) exit(1);
+
+    port_num = atoi(*&argv[1]);
 
     if (!(sock_fd = socket(AF_INET,SOCK_STREAM,0))){
            fprintf(stderr, "Error: Socket failed\n");
@@ -31,7 +34,7 @@ int main(int argc, char ** argv){
     client_addr.sin_family = AF_INET;
     client_addr.sin_port = htons(port_num);
     
-    if (inet_pton(AF_INET, "127.0.0.1", &client_addr.sin_addr)<=0)
+    if (inet_pton(AF_INET, *&argv[2], &client_addr.sin_addr)<=0)
     {
             fprintf(stderr,"Invalid address\n");
             exit(1);
@@ -71,4 +74,42 @@ void send_data (int client, char * msg, int msglen, int fl){
         fprintf(stderr, "Error sending data\n");
         exit(1);
     }
+}
+
+/* checks the validity of the command line arguments passed,  */
+/* returns -1 if invalid arguments are found, else returns 0  */    
+int arg_check(int argc, char *argv[]){
+
+
+    /* check if port number is entered */
+    if (argc != 3){
+       fprintf(stderr, "Error: Please provide port number\n");
+       fprintf(stdout, "Usage: ./Executable_name Port_number\n");
+       fprintf(stdout, "Server cannot be started\n");         
+       
+       return -1;
+    }
+
+    /* prevents segmentation faults if no arguments are given */
+    if (argc > 1){
+
+    /* check if integer is typed (only checks for numbers,  */
+    /* truncates any letters after the numbers)             */
+        if (isdigit(*argv[1]) == 0){
+            fprintf(stderr, "Error: Invalid port number\n");
+            fprintf(stdout, "Server cannot be started\n");
+            
+            return -1;       
+        }
+     
+     /* check port range 1 - 65535, 0 is not valid with telnet or netcat */
+        if ((atoi(argv[1]) < 1) || (atoi(argv[1]) > 65535)){
+            fprintf(stderr, "Error: Port number must be in range 1 - 65,535\n");
+            fprintf(stdout, "Server cannot be started\n"); 
+           
+            return -1;
+        }  
+    }
+    
+      return 0;
 }
